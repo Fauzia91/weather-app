c0f2697e7fd6834292dcc84139c1749d082e07f2
Fauzia <123041731+Fauzia91@users.noreply.github.com>
2023-03-15 21:58:16 -0400

    this is my readme.md

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # weather-app
+
+### Project Description
+
+A weather dashboard created using the Weather Api to help you plan your next trip. With the help of the API you can now retrieve current weather information as well as a 5 day forecast in desired cities nationwide.
+
+    
+
+### Table of Contents
+***
+
+[Deployed](#deployed)
+[Contributing](#contributing)
+[License](#license)
+[Github](#github)
+[Contact](#contact) 
+
+### Deployed
+
+https://fauzia91.github.io/weather-app/
+
+
+### Contributing
+
+If you have suggestions or references to help improve my portfolio for future projects please feel free to email me or put it in the issues.  
+
+### License
+
+MIT License
+
+Copyright (c) 2021 Fauzia Azad
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+### Github
+
+https://github.com/Fauzia91/weather-app
+
+### Contact
+
+Email: fauzia.azad13@gmail.com
---
+++ assets/script.js
@@ -0,0 +1,157 @@
+//two variables to attach to two elements, button variable with even listener 
+//click even listener will return city
+
+var buttonEl = document.querySelector("#searchBtn")
+var inputEl = document.querySelector("#inputSearch")
+var apiKey = "0c99b416b34685d632710484332adc13";
+var cityHistory = JSON.parse(localStorage.getItem("cityHistory")) || []
+var historyEl = document.querySelector("#history")
+
+function renderHistory() {
+    var createButton = document.createElement('button')
+    createButton.innerText= 'History'
+    document.body.appendChild(createButton);
+
+    //console.log(cityHistory)
+    for(let i= 0; i < cityHistory.length ; i++){
+        console.log(cityHistory[i])
+    }
+}
+//http://api.openweathermap.org/geo/1.0/direct?q=new+york&appid=0c99b416b34685d632710484332adc13
+//?key=value&key=value
+renderHistory()
+function currentWeather(){
+var city = inputEl.value;
+console.log ("q:",city)
+query = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`;
+
+
+
+// let cityList = [];
+// cityList.push(city)
+// localStorage.setItem("cityList", cityList);
+// cityList= localstorage.getItem("cityList");
+
+
+
+
+
+
+
+fetch(query)
+.then((response) => response.json()) //convert str to json
+.then((data) => {
+    let lat = data[0].lat;
+    let lon = data[0].lon;
+    var searchedCity = data[0].name
+    cityHistory.push(searchedCity)
+    localStorage.setItem("cityHistory", JSON.stringify(cityHistory))
+    console.log( data)
+    //fetch current weather
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`)
+    .then((response) => response.json()) //convert str to json
+    .then((data) => {
+        //console.log("current weather:", data)
+        /*weather conditions, the temperature, the humidity, and the the wind speed*/
+        //console.log(data.main.temp,data.main.humidity, data.wind.speed, data.weather[0].icon)
+        let weatherContainer = document.querySelector("#weatherContainer");
+        weatherContainer.textContent = "";
+
+        let tempEl = document.createElement("p");
+        tempEl.textContent = data.main.temp + "°F";
+
+
+        let humidityEl = document.createElement("p");
+        humidityEl.textContent = "humidity:" + data.main.humidity;
+
+        let windEl = document.createElement("p");
+        windEl.textContent = "wind speed:"+ data.wind.speed;
+
+        let imgEl = document.createElement("img");
+        imgEl.setAttribute("src", `https://openweathermap.org/themes/openweathermap/assets/vendor/owm/img/widgets/${data.weather[0].icon}.png
+        `)
+
+        
+        weatherContainer.appendChild(tempEl);
+        weatherContainer.appendChild(imgEl);
+        weatherContainer.appendChild(humidityEl);
+        weatherContainer.appendChild(windEl);
+
+
+
+        //five day forcast
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
+        ).then(response => response.json())
+        .then(data => {
+                console.log("fiveday:",data.list[0])
+                let fiveDayEl = document.querySelector("#fiveDayContainer");
+
+                for(let i= 0; i < 5 ; i++){
+                    console.log( data.list[(i * 8)] );//0,8,16,24,32
+                    let day = data.list[(i * 8)];
+
+                    let dateEl = document.createElement("p");
+                    dateEl.textContent = day.dt_txt;
+
+                    let tempEl = document.createElement("p");
+                    tempEl.textContent = day.main.temp + "°F";
+            
+            
+                    let humidityEl = document.createElement("p");
+                    humidityEl.textContent = "humidity:" + day.main.humidity;
+            
+                    let windEl = document.createElement("p");
+                    windEl.textContent = "wind speed:"+ day.wind.speed;
+            
+                    let imgEl = document.createElement("img");
+                    imgEl.setAttribute("src", `https://openweathermap.org/themes/openweathermap/assets/vendor/owm/img/widgets/${day.weather[0].icon}.png
+                    `)
+
+                    let divEl = document.createElement("div");
+
+                    divEl.appendChild(dateEl);
+                    divEl.appendChild(tempEl);
+                    divEl.appendChild(imgEl);
+                    divEl.appendChild(humidityEl);
+                    divEl.appendChild(windEl);
+
+                    fiveDayEl.appendChild(divEl)
+                }
+
+                
+
+
+
+
+
+
+
+
+        })
+
+
+
+
+
+
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+}
+
+buttonEl.addEventListener ("click",currentWeather)
